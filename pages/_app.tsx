a9d6e66ef48a8d4ffa5e989eb0d2d9b06f279dd0
Tom Bremer <tom@tbremer.com>
2020-11-03 12:06:51 -0600

    prettier cleanup - add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import Head from "next/head";
+import Head from 'next/head';
 import 'styles/style.css';
 
 export default function App({ Component, pageProps }) {
-    return (<>
-    <Head>
-    <link href="/favicon.png" rel="icon" />
-    </Head>
-    <Component {...pageProps} />
-    </>)
-  }
+  return (
+    <>
+      <Head>
+        <link href="/favicon.png" rel="icon" />
+        <title>Project 270 Electoral Tracker</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
