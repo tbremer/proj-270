1171afda37dfb1365d32c0bf4706b878c64d2de1
Tom Bremer <tom@tbremer.com>
2020-11-03 15:20:49 -0600

    meta information

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,17 +10,17 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <meta name="description" content="2020 Presidential Election Tracker" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://proj-270.tbremer.com/" />
+        <meta property="og:url" content="https://project270.us/" />
         <meta property="og:site_name" content="1990 to win" />
         <meta property="og:title" content="1,990 to win" />
         <meta property="og:description" content="2020 Presidential Election Tracker" />
-        <meta property="og:image" content="https://proj-270.tbremer.com/logo.png" />
+        <meta property="og:image" content="https://project270.us/logo.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:domain" value="https://proj-270.tbremer.com/" />
+        <meta name="twitter:domain" value="https://project270.us/" />
         <meta property="twitter:title" value="Project 270 Election Tracker" />
         <meta property="twitter:description" value="2020 Presidential Election Tracker" />
         <meta property="twitter:creator:id" value="_tbremer" />
-        <meta property="twitter:image" value="https://proj-270.tbremer.com/logo.png" />
+        <meta property="twitter:image" value="https://project270.us/logo.png" />
       </Head>
       <Component {...pageProps} />
     </>
