c9354a1e74f2593b264969cbbf854acbbad41a2a
Tom Bremer <tom@tbremer.com>
2020-11-03 12:49:09 -0600

    electoral votes, styling, credits

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,12 +65,37 @@ export default function Home({ data: states }: RunTimeProps) {
   return (
     <>
       <h1>2020 presidential Election Tracker</h1>
-      <hr />
+      <hr style={{ marginBottom: '2rem' }} />
 
       <TitledList title="Big six to watch" states={bigsix} />
       <TitledList title="Biden needs to win" states={biden} />
       <TitledList title="Trump needs to win" states={trump} />
       <TitledList title="Remaining states" states={rest} />
+
+      <hr />
+      <footer>
+        <h4>Credits:</h4>
+        <ul>
+          <li>
+            State JSON Data I was too lazy to type out:{' '}
+            <a href="//gist.github.com/mshafrir/2646763#file-states_titlecase-json">
+              gist.github/ mshafrir/states_hash.json
+            </a>
+          </li>
+          <li>
+            High level race data:{' '}
+            <a href="//www.vox.com/21428915/electoral-college-trump-biden-2020">
+              Vox: How Biden — or Trump — could win 270 electoral votes
+            </a>
+          </li>
+          <li>
+            Electoral Count:{' '}
+            <a href="//www.kare11.com/article/news/nation-world/how-many-electoral-votes-does-each-state-have-for-2020-presidential-election/507-7b3d52f4-ea66-4bbd-bfcf-3eb29f17586c">
+              KARE 11: How many electoral votes does each state get for presidential election?
+            </a>
+          </li>
+        </ul>
+      </footer>
     </>
   );
 }
